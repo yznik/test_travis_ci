273e45933b33506031cb5524dc7a43120ed56946
yznik <yznik93@gmail.com>
2020-02-19 00:47:09 +0300

    fix build, add uncrustify

--- ci/travis/install-linux.sh
+++ ci/travis/install-linux.sh
@@ -1,7 +1,7 @@
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
 
-sudo apt-get install -qq g++-8 make cmake libgtest-dev cppcheck sudo apt install clang-tools
+sudo apt-get install -qq g++-8 make cmake libgtest-dev cppcheck clang-tools
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 90
 cd /usr/src/gtest
 sudo cmake CMakeLists.txt
