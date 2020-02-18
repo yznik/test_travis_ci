b6f6b862df4d8893e1282b4d567cb99cd13bee16
yznik <yznik93@gmail.com>
2020-02-18 20:12:46 +0300

    use TRAVIS_BUILD_DIR

--- ci/travis/install-linux.sh
+++ ci/travis/install-linux.sh
@@ -3,10 +3,8 @@ sudo apt-get update -qq
 
 sudo apt-get install -qq g++-8 make cmake libgtest-dev
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 90
-ls -la
-pwd
 cd /usr/src/gtest
 sudo cmake CMakeLists.txt
 sudo make
 sudo cp *.a /usr/lib
-cd ~
+
